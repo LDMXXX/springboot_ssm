c367cb331d8c525bc0c0d6f1442c8f7cc2e130aa
itcast <hello@itcast.cn>
2021-11-03 21:01:39 +0800

    李昊修改

--- src/main/java/com/itheima/controller/LiHaoController.java
+++ src/main/java/com/itheima/controller/LiHaoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/li")
 public class LiHaoController {
 
+    @RequestMapping
     public String Lihao(){
         return "你好吗";
     }
